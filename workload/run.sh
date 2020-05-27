e91854ee41d9ea512c9381110b6266bdebda09d1
Lukas Wenzel <lukas.wenzel@hpi.de>
2020-05-27 17:40:19 +0200

    Fix cpu bind in run.sh

--- workload/run.sh
+++ workload/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-numactl -physcpubind=8-15 perf stat -I 500 ./workload $@ 2> ./output.log
+numactl --physcpubind=8-15 perf stat -I 500 ./workload $@ 2> ./output.log
 
 #watch -n 0.25 tail -n 11 ./output.log
 
